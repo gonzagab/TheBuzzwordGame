0e92b847287145028e1263629a80f2b38d811455
Bryant Gonzaga <bryant.gonzaga@stonybrook.edu>
2016-11-13 14:25:35 -0500

    learning how git and bitbucket work

---
+++ JFLAG/src/settings/InitializationParameters.java
@@ -0,0 +1,25 @@
+package settings;
+
+/**
+ * @author Ritwik Banerjee
+ */
+public enum InitializationParameters {
+
+    APP_PROPERTIES_XML("app-properties.xml"),
+    APP_ESP_PROPERTIES_XML("app-properties - esp.xml"),
+    WORKSPACE_PROPERTIES_XML("workspace-properties.xml"),
+    PROPERTIES_SCHEMA_XSD("properties-schema.xsd"),
+    ERROR_DIALOG_BUTTON_LABEL("Exit application."),
+    APP_WORKDIR_PATH("saved"),
+    APP_IMAGEDIR_PATH("images");
+
+    private String parameter;
+
+    InitializationParameters(String parameter) {
+        this.parameter = parameter;
+    }
+
+    public String getParameter() {
+        return parameter;
+    }
+}
