7b2cb68c314a8ddf78032b0998615d3ae31d057b
Bryant Gonzaga <bryant.gonzaga@stonybrook.edu>
2016-11-13 16:26:10 -0500

    doing some more test to learn how to use version control

---
+++ Buzzword/src/buzzword/Buzzword.java
@@ -0,0 +1,38 @@
+package buzzword;
+
+import apptemplate.AppTemplate;
+import components.AppComponentsBuilder;
+import components.AppDataComponent;
+import components.AppFileComponent;
+import components.AppWorkspaceComponent;
+
+
+/**
+ * Created by Bryant Gonzaga on 11/13/2016.
+ */
+public class Buzzword extends AppTemplate
+{
+    public AppComponentsBuilder makeAppBuilderHook()
+    {
+        return new AppComponentsBuilder() {
+            @Override
+            public AppDataComponent buildDataComponent() throws Exception {
+                return null;
+            }
+
+            @Override
+            public AppFileComponent buildFileComponent() throws Exception {
+                return null;
+            }
+
+            @Override
+            public AppWorkspaceComponent buildWorkspaceComponent() throws Exception {
+                return null;
+            }
+        };
+    }
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
